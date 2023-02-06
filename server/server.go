8c05d2cf4e209b75a61dcfc5149bd7872ead6767
lithium <lithium@tsunagatte.ru>
2023-02-06 22:18:44 +0500

    Moved handlers to a separate file

--- server/server.go
+++ server/server.go
@@ -1,15 +1,9 @@
 package server
 
 import(
-	"log"
-	"net/http"
 	"database/sql"
-	"strconv"
-	"io/ioutil"
-	"encoding/json"
 	
 	"github.com/gin-gonic/gin"
-	"github.com/tsunagatteru/ishiki-no-nagare/db"
 	"github.com/tsunagatteru/ishiki-no-nagare/model"
 )
 
@@ -44,45 +38,3 @@ func ConfigMiddleware(config *model.Config) gin.HandlerFunc {
 	}
 }
 
-
-func showPosts(c *gin.Context){
-	page := "0"
-	page = (c.Param("page"))
-	res, err := http.Get(c.MustGet("BaseURL").(string) + c.MustGet("ApiDir").(string) + "/posts/" + page)
-	if err != nil {
-		log.Println(err)
-	}
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil{
-		log.Println(err)
-	}
-	var response []model.Post
-	json.Unmarshal(body, &response)
-	c.HTML(http.StatusOK, "posts.tmpl", gin.H{
-		"Posts": response,
-	})
-}
-
-func getPosts(c *gin.Context){
-	pageNumber := 0
-	pageNumber, err := strconv.Atoi(c.Param("page"))
-	if err != nil{
-		log.Println(err)
-	}
-	pageLength := c.MustGet("PageLength").(int)
-	dbConn := c.MustGet("dbConn").(*sql.DB)
-	posts := db.RetrievePage(dbConn, pageNumber, pageLength)
-	c.JSON(http.StatusOK, posts)
-}
-
-func createPost(c *gin.Context){
-	messageByte, err := c.GetRawData()
-	if err != nil {
-		log.Println(err)
-	}
-	message := string(messageByte)
-	dbConn := c.MustGet("dbConn").(*sql.DB)
-	db.AddPost(dbConn, message)
-	c.IndentedJSON(http.StatusCreated, message)
-}
