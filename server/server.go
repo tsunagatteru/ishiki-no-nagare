6e8c00f44e6c945c995c6df3f140fe9cbc490fce
lithium <lithium@tsunagatte.ru>
2023-02-06 19:00:44 +0500

    Simple frontend

--- server/server.go
+++ server/server.go
@@ -5,10 +5,12 @@ import(
 	"net/http"
 	"database/sql"
 	"strconv"
+	"io/ioutil"
+	"encoding/json"
 	
 	"github.com/gin-gonic/gin"
 	"github.com/tsunagatteru/ishiki-no-nagare/db"
-	_ "github.com/tsunagatteru/ishiki-no-nagare/model"
+	"github.com/tsunagatteru/ishiki-no-nagare/model"
 )
 
 func NewRouter()(*gin.Engine){
@@ -17,10 +19,14 @@ func NewRouter()(*gin.Engine){
 }
 
 func RunRouter(router *gin.Engine, dbConn *sql.DB){
+	router.LoadHTMLGlob("res/templates/*.tmpl")
 	router.Use(DatabaseMiddleware(dbConn))
+	router.Use(ConfigMiddleware())
 	api := router.Group("/api")
 	api.GET("/posts/:page", getPosts)
 	api.POST("/create-post", createPost)
+	front := router.Group("/")
+	front.GET("/posts/:page", showPosts)
 	router.Run("localhost:8080")
 }
 
@@ -30,7 +36,35 @@ func DatabaseMiddleware(dbConn *sql.DB) gin.HandlerFunc {
 	}
 }
 
+func ConfigMiddleware() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		c.Set("BaseURL", "http://localhost:8080")
+		c.Set("ApiDir", "/api")
+	}
+}
+
+
+func showPosts(c *gin.Context){
+	page := "0"
+	page = (c.Param("page"))
+	res, err := http.Get(c.MustGet("BaseURL").(string) + c.MustGet("ApiDir").(string) + "/posts/" + page)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil{
+		log.Println(err)
+	}
+	var response []model.Post
+	json.Unmarshal(body, &response)
+	c.HTML(http.StatusOK, "posts.tmpl", gin.H{
+		"Posts": response,
+	})
+}
+
 func getPosts(c *gin.Context){
+	pageNumber := 0
 	pageNumber, err := strconv.Atoi(c.Param("page"))
 	if err != nil{
 		log.Println(err)
